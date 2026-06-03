35c81b4bc6f24f14dd4ae6aaaae4b9e071d307f1
Ilya <ilya_0836@mail.ru>
2026-06-03 15:31:44 +0300

    refactor: deleted unnecessary imports

--- schemas/base.py
+++ schemas/base.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel, Field
 from typing import Optional
-from datetime import datetime
 
 class NameSchema(BaseModel):
     name: str = Field(..., min_length=1, max_length=150)
