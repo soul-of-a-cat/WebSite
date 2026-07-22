bfbf44cd3595d1c097fca4f561cf94649912a94c
Ilya <ilya_0836@mail.ru>
2026-07-22 16:49:03 +0300

    feature: added database creation, router download, and application launch

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI, Request
 from starlette.staticfiles import StaticFiles
+from contextlib import asynccontextmanager
 
 from core.config import settings
-from fastapi.templating import Jinja2Templates
+from api.routes.posts import router as posts_router
+from api.routes.homepage import router as homepage_router
+from core.database import engine, Base
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    await engine.dispose()
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -10,13 +22,16 @@ app = FastAPI(
     version=settings.APP_VERSION,
 )
 
-templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse(
-        request=request,
-        name="homepage/main.html",
-        context={}
+app.include_router(posts_router)
+app.include_router(homepage_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        host=settings.HOST,
+        port=settings.PORT,
+        reload=settings.DEBUG
     )
