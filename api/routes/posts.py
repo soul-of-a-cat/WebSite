452b04c61b667c941288b35406dc0f987c2de222
Ilya <ilya_0836@mail.ru>
2026-08-06 16:42:17 +0300

    feature: added a check for the correct user

--- api/routes/posts.py
+++ api/routes/posts.py
@@ -7,8 +7,11 @@ from starlette.responses import RedirectResponse, HTMLResponse
 
 from core.database import get_async_session
 from core.templates import templates
+from models.user import User
 from services.post_service import PostService
 from forms.post_form import PostCreateForm, PostUpdateForm
+
+from api.dependencies.auth import get_current_user
 
 router = APIRouter(
     prefix="/posts",
@@ -16,7 +19,9 @@ router = APIRouter(
 )
 
 @router.get("/create", response_class=HTMLResponse, name="create_post")
-async def create_post_page(request: Request):
+async def create_post_page(
+        request: Request
+):
     return templates.TemplateResponse(
         request=request,
         name="posts/post_create.html",
@@ -26,13 +31,15 @@ async def create_post_page(request: Request):
 @router.post("/create", name="create_post")
 async def create_post(
         form: Annotated[PostCreateForm, Depends()],
-        session: AsyncSession = Depends(get_async_session)
+        session: AsyncSession = Depends(get_async_session),
+        current_user: User = Depends(get_current_user)
 ):
     data = await form.validate()
     await PostService.create(
         session=session,
         data=data,
         images=form.images,
+        user=current_user
     )
     return RedirectResponse(
         url="/",
@@ -67,17 +74,23 @@ async def delete_post(
 @router.post("/delete/{post_id}", name="delete_post")
 async def delete_post(
         post_id: int,
-        session: AsyncSession = Depends(get_async_session)
+        session: AsyncSession = Depends(get_async_session),
+        current_user: User = Depends(get_current_user)
 ):
     post = await PostService.get_by_id(session, post_id)
-    await PostService.delete(session, post)
+    await PostService.delete(
+        session=session,
+        post=post,
+        user=current_user
+    )
     return RedirectResponse("/")
 
 @router.post("/update/{post_id}", name="update_post")
 async def update_post(
         post_id: int,
         form: Annotated[PostUpdateForm, Depends()],
-        session: AsyncSession = Depends(get_async_session)
+        session: AsyncSession = Depends(get_async_session),
+        current_user: User = Depends(get_current_user)
 ):
     data = await form.validate()
     await PostService.update(
@@ -86,6 +99,7 @@ async def update_post(
         data=data,
         deleted_images_ids=form.deleted_image_ids,
         new_images=form.new_images,
+        user=current_user
     )
 
     return RedirectResponse(
